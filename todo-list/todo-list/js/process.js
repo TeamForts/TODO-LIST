aceb9928919799114c99181d9eb126056f0f4a89
TeamForts <79396887+TeamForts@users.noreply.github.com>
2021-08-06 17:11:23 +0300

    new Changes

--- todo-list/todo-list/js/process.js
+++ todo-list/todo-list/js/process.js
@@ -15,6 +15,7 @@ let selOpt = 3;
 let massiveTasks = [];
 let newmas = [];
 
+const addForm = document.forms.addForm;
 
 function getTime() {
 	let today = new Date();
@@ -23,43 +24,136 @@ function getTime() {
 }
 
 function addTask() {
-	task.text = document.getElementById("form_text").value;
-	task.priority = document.getElementById("form_priority").value;
+	task.priority = addForm.elements[0].value;
+	task.text = addForm.elements[1].value;
 	task.time = getTime();
 	return task;
 }
+const getResource = async (url) => {
+	return fetch(url)
+		.then(result => result.json())
+		.then(result => {
+			massiveTasks = result;
+			outputMassive(result);
+		});
+}
 
-function changeColor(done, id,mas) {
+async function sendResource(url, task) {
+	fetch(url, {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json;charset=utf-8'
+		},
+		body: JSON.stringify(task)
+	})
+		.then(result => result.json())
+		.then(result => {
+			massiveTasks.push(result);
+			outputMassive(massiveTasks);
+		});
+}
+
+async function putResource(url, newTask) {
+	fetch(url, {
+		method: 'PUT',
+		headers: {
+			'Content-Type': 'application/json;charset=utf-8'
+		},
+		body: JSON.stringify(newTask)
+	}).then(result => result.json())
+		.then(result => {
+			outputMassive(massiveTasks);
+		});
+}
+
+async function deleteResource(url) {
+	fetch(url, {
+		method: 'DELETE',
+	})
+		.then(result => result.json())
+		.then(result => {
+			massiveTasks.forEach(function (item,index) {
+				if (item.id === result.id)
+					massiveTasks.splice(index,1);
+			})
+			outputMassive(massiveTasks);
+		})
+}
+function changeColor(done, index,mas) {
 	if (done == 1) {
-		document.getElementById('done_' + id).style.display = "none";
-		document.getElementById('none_done_' + id).style.display = "flex";
-		document.getElementById(`task_${id}`).style.backgroundColor = "#3CB371";
-		document.getElementById(`textarea_${id}`).style.backgroundColor = "#3CB371";
-		document.getElementById(`task_priority_${id}`).style.color = "#3CB371";
+		document.getElementById(`done_${index}`).style.display = "none";
+		document.getElementById(`none_done_${index}`).style.display = "flex";
+		document.getElementById(`task_${index}`).style.backgroundColor = "#3CB371";
+		document.getElementById(`textarea_${index}`).style.backgroundColor = "#3CB371";
+		document.getElementById(`task_priority_${index}`).style.color = "#3CB371";
 	}
 	if (done == 2) {
-		document.getElementById('none_done_' + id).style.display = "none";
-		document.getElementById('done_' + id).style.display = "flex";
-		document.getElementById(`task_${id}`).style.backgroundColor = "#F08080";
-		document.getElementById(`textarea_${id}`).style.backgroundColor = "#F08080";
-		document.getElementById(`task_priority_${id}`).style.color = "#F08080";
+		document.getElementById(`none_done_${index}`).style.display = "none";
+		document.getElementById(`done_${index}`).style.display = "flex";
+		document.getElementById(`task_${index}`).style.backgroundColor = "#F08080";
+		document.getElementById(`textarea_${index}`).style.backgroundColor = "#F08080";
+		document.getElementById(`task_priority_${index}`).style.color = "#F08080";
 	}
 	if (done == 0){
-		if (mas[id].priority === 'низкий')
+		if (mas[index].priority === 'низкий')
 		{
-			document.getElementById(`task_priority_${id}`).style.color = "violet";
+			document.getElementById(`task_priority_${index}`).style.color = "violet";
 		}
-		if (mas[id].priority === 'средний')
+		if (mas[index].priority === 'средний')
 		{
-			document.getElementById(`task_priority_${id}`).style.color = "orange";
+			document.getElementById(`task_priority_${index}`).style.color = "orange";
 		}
-		if (mas[id].priority === 'высокий')
+		if (mas[index].priority === 'высокий')
 		{
-			document.getElementById(`task_priority_${id}`).style.color = "blue";
+			document.getElementById(`task_priority_${index}`).style.color = "blue";
 		}
 	}
 }
+function changeActive() {
+	active = !active;
+	newmas = massiveTasks;
+	newmas = filterMasPriority(newmas, selOpt);
+	newmas = filterMasComplete(newmas);
+	outputMassive(newmas);
+}
+function changeEnd() {
+	end = !end;
+	newmas = massiveTasks;
+	newmas = filterMasPriority(newmas, selOpt);
+	newmas = filterMasComplete(newmas);
+	outputMassive(newmas);
+}
+function changeCancel() {
+	cancel = !cancel;
+	newmas = massiveTasks;
+	newmas = filterMasPriority(newmas, selOpt);
+	newmas = filterMasComplete(newmas);
+	outputMassive(newmas);
+}
+function changeFilterButton(select) {
+	selectedOption = select.options[select.selectedIndex];
+	selOpt = selectedOption.value
+	newmas = filterMasPriority(massiveTasks, selOpt);
+	newmas = filterMasComplete(newmas);
+	outputMassive(newmas);
 
+}
+function filterMasText() {
+	let item = document.getElementById('search').value;
+	if (item.length > 2) {
+		newmas = newmas.filter(function (txt) {
+			for (i = 0; i < item.length; i++) {
+				if (txt.text[i] === item[i]) {
+					return txt;
+				}
+			}
+		})
+	}
+	if (item.length <= 2) {
+		newmas = massiveTasks;
+	}
+	outputMassive(newmas);
+}
 function changeClassSort(id, toggle) {
 	if (id)
 	{
@@ -89,88 +183,6 @@ function changeClassSort(id, toggle) {
 		}
 	}
 }
-
-function clickSortButtonDate() {
-	toggle_date = !toggle_date;
-	changeClassSort(false,toggle_date);
-	outputMassive(massiveTasks);
-}
-
-function clickSortButtonPriority() {
-	toggle_priority = !toggle_priority;
-	changeClassSort(true,toggle_priority);
-	outputMassive(massiveTasks);
-}
-
-const getResourse = async (url) => {
-	return await fetch(url)
-	.then(result => result.json());
-}
-
-async function sendResourse(url, task) {
-	let response = await fetch(url, {
-		method: 'POST',
-		headers: {
-			'Content-Type': 'application/json;charset=utf-8'
-		},
-		body: JSON.stringify(task)
-	}).then(result => result.json());
-}
-
-async function putResourse(url, newTask) {
-	let response = await fetch(url, {
-		method: 'PUT',
-		headers: {
-			'Content-Type': 'application/json;charset=utf-8'
-		},
-		body: JSON.stringify(newTask)
-	}).then(result => result.json()
-	.then((result)=> {
-	localStorage.clear;
-	console.log('result ' + result);
-	localStorage.setItem('myTasks', JSON.stringify(massiveTasks));
-	}));
-}
-
-async function deleteResourse(url) {
-	let response = await fetch(url, {
-		method: 'DELETE',
-	});
-}
-
-function outputMassive(mas) {
-	let out_arr = document.getElementById('out_arr');
-	document.getElementById('out_arr').innerHTML = '';
-	for (let i = 0; i < mas.length; i++) {
-		if (mas[i] !== undefined) {
-			out_arr.innerHTML +=
-				"<div class='task'" + ">" +
-					"<div id='task_priority_" + i + "' class='task_priority'>" +
-						mas[i].priority +
-					"</div>" +
-					"<div id='task_" + i + "' class='task_content'>" +
-						"<div class='task_top'>" +
-							"<textarea class='task_text' rows='5'" + "id='textarea_" + i + "'onblur='changeText(" + i + ")'>" +
-								mas[i].text +
-							"</textarea>" +
-							"<div class='task_interactive'>" +
-								"<div class='done' id='done_" + i + "' onclick='changeOnDone(" + i + ")'>" + "</div>" +
-								"<div id='none_done_" + i + "' onclick='changeOnNoneDone(" + i + ")' class='none_done'>" +	"</div>" +
-							"</div>" +
-						"</div>" +
-						"<div class='task_date'>" +
-							mas[i].time +
-						"</div>" +
-					"</div>" +
-					"<div class='task_delete' onclick='deleteTask(" + i + ")'>" +
-					"</div>" +
-				"</div>";
-			changeColor(mas[i].complete, i,mas);
-		}
-
-	}
-}
-
 function filterMasPriority(mas, select) {
 	let newMas = [];
 
@@ -192,7 +204,6 @@ function filterMasPriority(mas, select) {
 	if (select == 3) { newMas = massiveTasks }
 	return newMas;
 }
-
 function sortMasPriority(mas, select) {
 	let newMas = [];
 	if (select) {
@@ -227,7 +238,6 @@ function sortMasPriority(mas, select) {
 	}
 	return newMas;
 }
-
 function filterMasComplete(mas) {
 	let newMas = mas;
 
@@ -273,137 +283,85 @@ function sortMasData(mas, select) {
 	return mas;
 }
 
+function clickSortButtonDate() {
+	toggle_date = !toggle_date;
+	changeClassSort(false,toggle_date);
+	outputMassive(massiveTasks);
+}
 
-function changeText(id) {
+function clickSortButtonPriority() {
+	toggle_priority = !toggle_priority;
+	changeClassSort(true,toggle_priority);
+	outputMassive(massiveTasks);
+}
+function outputMassive(mas) {
+	let out_arr = document.getElementById('out_arr');
+	document.getElementById('out_arr').innerHTML = '';
+	for (let i = 0; i < mas.length; i++) {
+		if (mas[i] !== undefined) {
+			out_arr.innerHTML +=
+				`
+				<div class="task">
+					<div id="task_priority_${i}" class="task_priority">
+						${mas[i].priority}
+					</div>
+					 <div id="task_${i}" class="task_content">
+						<div class="task_top">
+							<textarea class='task_text' rows='5' id="textarea_${i}" onblur="changeText(${mas[i].id},${i})">${mas[i].text}</textarea>
+							<div class="task_interactive">
+								<div class="done" id="done_${i}" onclick="changeOnDone(${mas[i].id},${i})"></div>
+								<div id="none_done_${i}" onclick="changeOnNoneDone(${mas[i].id},${i})" class="none_done"></div>				
+							</div> 
+						</div>
+						<div class="task_date"> 
+							${mas[i].time}
+						</div>
+					</div>
+					<div class="task_delete" onclick="deleteTask(${mas[i].id})">
+					</div>
+				</div>`;
+			changeColor(mas[i].complete, i, mas);
+		}
+	}
+}
+function changeText(id,index) {
 	let result = confirm('Сохраняем изменение?');
 	if (result) {
-		let textChange = document.getElementById('textarea_' + id);
-		massiveTasks[id].text = textChange.value;
-		putResourse(`http://127.0.0.1:3000/items/${id}`, massiveTasks[id]);
-		getRosponseFromServer();
+		let textChange = document.getElementById(`textarea_${index}`);
+		massiveTasks[index].text = textChange.value;
+		putResource(`http://127.0.0.1:3000/items/${id}`, massiveTasks[index]);
 	}
 }
 
+function changeOnDone(id,index) {
+	massiveTasks[index].complete = 1;
+	changeColor(massiveTasks[index].complete, index);
+	putResource(`http://127.0.0.1:3000/items/${id}`, massiveTasks[index]);
+}
+
+function changeOnNoneDone(id, index) {
+	massiveTasks[index].complete = 2;
+	changeColor(massiveTasks[index].complete, index);
+	putResource(`http://127.0.0.1:3000/items/${id}`, massiveTasks[index]);
+}
 function deleteTask(id) {
-	deleteResourse(`http://127.0.0.1:3000/items/${id}`);
-	getRosponseFromServer();
+	deleteResource(`http://127.0.0.1:3000/items/${id}`);
 }
 
-function changeOnDone(id) {
-	massiveTasks[id].complete = 1;
-	changeColor(massiveTasks[id].complete, id);
-	putResourse(`http://127.0.0.1:3000/items/${id}`, massiveTasks[id]);
-	localStorage.clear;
-	localStorage.setItem('myTasks', JSON.stringify(massiveTasks));
-	getRosponseFromServer();
-}
-
-function changeOnNoneDone(id) {
-	massiveTasks[id].complete = 2;
-	changeColor(massiveTasks[id].complete, id);
-	putResourse(`http://127.0.0.1:3000/items/${id}`, massiveTasks[id]);
-	localStorage.clear;
-	localStorage.setItem('myTasks', JSON.stringify(massiveTasks));
-	getRosponseFromServer();
-}
-
-function changeActive() {
-	active = !active;
-	newmas = massiveTasks;
-	newmas = filterMasPriority(newmas, selOpt);
-	newmas = filterMasComplete(newmas);
-	outputMassive(newmas);
-}
-function changeEnd() {
-	end = !end;
-	newmas = massiveTasks;
-	newmas = filterMasPriority(newmas, selOpt);
-	newmas = filterMasComplete(newmas);
-	outputMassive(newmas);
-}
-function changeCancel() {
-	cancel = !cancel;
-	newmas = massiveTasks;
-	newmas = filterMasPriority(newmas, selOpt);
-	newmas = filterMasComplete(newmas);
-	outputMassive(newmas);
-}
-
- 
-function changeFilterButton(select) {
-
-	selectedOption = select.options[select.selectedIndex];
-	selOpt = selectedOption.value
-	newmas = filterMasPriority(massiveTasks, selOpt);
-	newmas = filterMasComplete(newmas);
-	outputMassive(newmas);
-
-}
-
-
-function filterMasText() {
-	let item = document.getElementById('search').value;
-	if (item.length > 2) {
-		newmas = newmas.filter(function (txt) {
-			for (i = 0; i < item.length; i++) {
-				if (txt.text[i] === item[i]) {
-					return txt;
-				}
-			}
-		})
-	}
-	if (item.length <= 2) {
-		newmas = massiveTasks;
-	}
-	outputMassive(newmas);
-}
 function sendToServer() {
-	if (document.getElementById('form_text').value != '') {
+	if (addForm.elements[1].value != '') {
 		let task = addTask();
-		sendResourse('http://127.0.0.1:3000/items', task)
-		getRosponseFromServer();
-		document.getElementById('form_text').value = ''
+		sendResource('http://127.0.0.1:3000/items', task)
+		addForm.elements[1].value = '';
 	}
 	else {
 		alert('Введите текст');
 	}
 }
-function getRosponseFromServer() {
-	getResourse('http://127.0.0.1:3000/items').then((data) => {
-		localStorage.setItem('myTasks', JSON.stringify(data));
-		massiveTasks = data;
-		outputMassive(data);
-	});
-}
-
 { 
 	document.getElementById('submitButton').addEventListener('click', sendToServer);
-	getRosponseFromServer();
-	massiveTasks = JSON.parse(localStorage.getItem('myTasks'));
-	newmas = massiveTasks;
-	outputMassive(newmas);
-
+	newmas = getResource('http://127.0.0.1:3000/items');
 }
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*textarea.addEventListener('textarea', function (e) {
-	textarea.value = textarea.value.replace(/(\r\n|\n|\r)/gm, "");
-})
-document.querySelector('textarea').addEventListener('input', function (e) {
-	e.target.style.height = 'auto'
-	e.target.style.height = e.target.scrollHeight + 2 + "px"
-})*/
